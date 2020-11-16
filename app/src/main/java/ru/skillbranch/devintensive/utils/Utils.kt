ffb9d94aa97df52c2c6ecdbd08f6c7289fe1fc59
ldrmomentpro <73545742+ldrmomentpro@users.noreply.github.com>
2020-11-17 01:37:38 +0600

    hometask_2 parseFullname

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,91 +4,99 @@ import java.lang.StringBuilder
 import java.util.*
 
 object Utils {
+
+    private val translitMap = mapOf("А" to "A",
+        "Б" to "B",
+        "В" to "V",
+        "Г" to "G",
+        "Д" to "D",
+        "Е" to "E",
+        "Ё" to "E",
+        "Ж" to "Zh",
+        "З" to "Z",
+        "И" to "I",
+        "Й" to "I",
+        "К" to "K",
+        "Л" to "L",
+        "М" to "M",
+        "Н" to "N",
+        "О" to "O",
+        "П" to "P",
+        "Р" to "R",
+        "С" to "S",
+        "Т" to "T",
+        "У" to "U",
+        "Ф" to "F",
+        "Х" to "H",
+        "Ц" to "C",
+        "Ч" to "Ch",
+        "Ш" to "Sh",
+        "Щ" to "Sh'",
+        "Ъ" to "",
+        "Ы" to "I",
+        "Ь" to "",
+        "Э" to "E",
+        "Ю" to "Yu",
+        "Я" to "Ya",
+        "а" to "a",
+        "б" to "b",
+        "в" to "v",
+        "г" to "g",
+        "д" to "d",
+        "е" to "e",
+        "ё" to "e",
+        "ж" to "zh",
+        "з" to "z",
+        "и" to "i",
+        "й" to "i",
+        "к" to "k",
+        "л" to "l",
+        "м" to "m",
+        "н" to "n",
+        "о" to "o",
+        "п" to "p",
+        "р" to "r",
+        "с" to "s",
+        "т" to "t",
+        "у" to "u",
+        "ф" to "f",
+        "х" to "h",
+        "ц" to "c",
+        "ч" to "ch",
+        "ш" to "sh",
+        "щ" to "sh'",
+        "ъ" to "",
+        "ы" to "i",
+        "ь" to "",
+        "э" to "e",
+        "ю" to "yu",
+        "я" to "ya")
     fun parseFullName(fullName: String?) : Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.split(" ")
 
-        val parts: List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        var firstName: String?
+        var lastName: String?
 
+        if (fullName.isNullOrBlank() || fullName=="" || fullName==" ") {
+            firstName=null
+            lastName=null
+        }
+        else {
+            firstName= parts?.getOrNull(0)
+            lastName= parts?.getOrNull(1)
+            if (lastName.isNullOrBlank()) lastName=null
+        }
         return firstName to lastName
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        val letters = mapOf(
-            "А" to "A",
-            "Б" to "B",
-            "В" to "V",
-            "Г" to "G",
-            "Д" to "D",
-            "Е" to "E",
-            "Ё" to "E",
-            "Ж" to "Zh",
-            "З" to "Z",
-            "И" to "I",
-            "Й" to "I",
-            "К" to "K",
-            "Л" to "L",
-            "М" to "M",
-            "Н" to "N",
-            "О" to "O",
-            "П" to "P",
-            "Р" to "R",
-            "С" to "S",
-            "Т" to "T",
-            "У" to "U",
-            "Ф" to "F",
-            "Х" to "H",
-            "Ц" to "C",
-            "Ч" to "Ch",
-            "Ш" to "Sh",
-            "Щ" to "Sh'",
-            "Ъ" to "",
-            "Ы" to "I",
-            "Ь" to "",
-            "Э" to "E",
-            "Ю" to "Yu",
-            "Я" to "Ya",
-            "а" to "a",
-            "б" to "b",
-            "в" to "v",
-            "г" to "g",
-            "д" to "d",
-            "е" to "e",
-            "ё" to "e",
-            "ж" to "zh",
-            "з" to "z",
-            "и" to "i",
-            "й" to "i",
-            "к" to "k",
-            "л" to "l",
-            "м" to "m",
-            "н" to "n",
-            "о" to "o",
-            "п" to "p",
-            "р" to "r",
-            "с" to "s",
-            "т" to "t",
-            "у" to "u",
-            "ф" to "f",
-            "х" to "h",
-            "ц" to "c",
-            "ч" to "ch",
-            "ш" to "sh",
-            "щ" to "sh'",
-            "ъ" to "",
-            "ы" to "i",
-            "ь" to "",
-            "э" to "e",
-            "ю" to "yu",
-            "я" to "ya"
-        )
 
         val text = payload
         val sb = StringBuilder(text.length)
         for (i in 0 until text.length) {
             val l: String = payload.substring(i, i + 1)
-            if (letters.containsKey(l)) {
-                sb.append(letters[l])
+            if (translitMap.containsKey(l)) {
+                sb.append(translitMap[l])
             } else {
                 sb.append(l)
             }
