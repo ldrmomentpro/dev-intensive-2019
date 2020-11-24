c2052ca768c9187b23ff1ca0735e74d065cce670
ldrmomentpro <73545742+ldrmomentpro@users.noreply.github.com>
2020-11-24 20:09:30 +0600

    hometask_4 task splash

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -37,7 +37,7 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //TODO set custom Theme this before super and setContentView
 
-//        setTheme(R.style.AppTheme)
+        setTheme(R.style.AppTheme)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
